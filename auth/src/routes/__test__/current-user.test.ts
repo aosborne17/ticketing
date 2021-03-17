8fbf029c61aa4b31706b4efb0fac8e67c9ba27fb
Andrew <andrew@conducttr.com>
2021-03-17 18:50:09 +0000

    changing current user tests

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,24 +1,24 @@
-import request from 'supertest';
-import { app } from '../../app';
+import request from "supertest";
+import { app } from "../../app";
 
 // after creating an account, passing in an incorrect password to signin should fail
-it('Responds with details about the current user', async () => {
+it("Responds with details about the current user", async () => {
   const cookie = await global.signin();
   const response = await request(app)
-    .get('/api/users/currentuser')
-    .set('Cookie', cookie) // the .set() allows us to add headers to our requests
+    .get("/api/users/currentuser")
+    .set("Cookie", cookie) // the .set() allows us to add headers to our requests
     .send()
-    .expect(200);
+    .expect(400);
 
   // making sure the req email in our currentUser is the same as our signin email
-  expect(response.body.currentUser.email).toEqual('test@test.com');
+  expect(response.body.currentUser.email).toEqual("test@test.com");
 });
 
 // current user should return null if user is not logged in
-it('Responds with current user=null if not authenticated', async () => {
+it("Responds with current user=null if not authenticated", async () => {
   // const cookie = await global.signin();
   const response = await request(app)
-    .get('/api/users/currentuser')
+    .get("/api/users/currentuser")
     .send()
     .expect(200);
 
